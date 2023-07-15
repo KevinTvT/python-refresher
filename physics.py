cb3ea416d79ff8db6f54fd5fec007ae0a9096263
KevinTvT <kevindnie@gmail.com>
2023-07-14 21:13:31 -0400

    Added up to problem 9 for physics and tests for it

--- physics.py
+++ physics.py
@@ -1,3 +1,13 @@
+import math
+import numpy as np
+
+"""CREATED A ROUNDING FUNCTION TO THREE DECIMAL PLACES"""
+
+
+def myRound(n):
+    return round(n, 3)
+
+
 """Problem 1:
 Write a Python function, calculate_buoyancy, that calculates the buoyancy force exerted on a object submerged in water.
 The function should take the following arguments:
@@ -9,6 +19,8 @@ g = 9.81  # m/s^2
 
 
 def calculate_buoyancy(V, density_fluid):
+    if V < 0:
+        raise ValueError("Volume cannot be less than 0")
     return V * density_fluid * g  # Newtons or kg*m/s^2
 
 
@@ -22,7 +34,11 @@ water_density = 1000  # kg/m^3
 
 
 def will_it_float(V, mass):
-    return True if V * water_density > mass else False
+    if V < 0:
+        raise ValueError("Volume cannot be less than 0")
+    if V * water_density == mass:
+        return "NEUTRAL BUOYANCY"
+    return V * water_density > mass
 
 
 """Problem 3:
@@ -35,4 +51,155 @@ The function should return the pressure in Pascals."""
 
 
 def calculate_pressure(depth):
-    return water_density * g * depth  # Pascals or N/m^2
+    surface = 101325
+    if depth <= 0:
+        raise ValueError("Volume cannot be less than 0")
+    return water_density * g * depth + surface  # Pascals or N/m^2
+
+
+"""Problem 4
+Write a Python function, calculate_acceleration, that calculates the acceleration of an object given the force applied to it and its mass.
+The function should take the following arguments:
+F: the force applied to the object in Newtons,
+m: the mass of the object in kilograms.
+"""
+
+
+def calculate_acceleration(force, mass):
+    if mass <= 0:
+        raise ValueError("Mass cannot be less than or equal to 0")
+    return force / mass
+
+
+"""Problem 5
+Write a Python function, calculate_angular_acceleration, that calculates the angular acceleration of an object given the torque applied to it and its moment of inertia.
+The function should take the following arguments:
+tau: the torque applied to the object in Newton-meters,
+I: the moment of inertia of the object in kg/m^2"""
+
+
+def calculate_angular_acceleration(tau, inertia):
+    if inertia <= 0:
+        raise ValueError("Inertia cannot be less than or equal to 0")
+    return tau / inertia
+
+
+"""Problem 6
+Write a Python function, calculate_torque, that calculates the torque applied to an object given the force applied to it and the distance from the axis of rotation to the point where the force is applied.
+The function should take the following arguments:
+F_magnitude: the magnitude of force applied to the object in Newtons,
+F_direction: the direction of the force applied to the object in degrees,
+r: the distance from the axis of rotation to the point where the force is applied in meters."""
+
+
+def calculate_torque(F_magnitude, F_direction, r):
+    if r <= 0:
+        raise ValueError("Distance is a magnitude cannot be negative")
+    return myRound(F_magnitude * r * math.sin(math.radians(F_direction)))
+
+
+"""Problem 7
+Write a Python function, calculate_moment_of_inertia, that calculates the moment of inertia of an object given its mass and the distance from the axis of rotation to the center of mass of the object.
+The function should take the following arguments:
+m: the mass of the object in kilograms,
+r: the distance from the axis of rotation to the center of mass of the object in meters."""
+
+
+def calculate_moment_of_inertia(mass, r):
+    if mass <= 0:
+        raise ValueError("Mass cannot be negative or 0")
+    return mass * r**2
+
+
+"""Problem 8 (part 1)
+An AUV is submerged in water. The AUV has a mass of 100 kg and a volume of 0.1 m^3
+The AUV is equipped with a thruster that can apply a force up to 100N. The thruster is located 0.5m from the center of mass of the AUV. The thruster can rotate 30 degrees in either direction of the x-axis.
+
+Write a Python function, calculate_auv_acceleration, that calculates the acceleration of the AUV in the 2D plane.
+The function should take the following arguments:
+F_magnitude: the magnitude of force applied by the thruster in Newtons.
+F_angle: the angle of the force applied by the thruster in radians. The angle is measured from the x-axis. Positive angles are measured in the counter-clockwise direction.
+mass (optional): the mass of the AUV in kilograms. The default value is 100kg.
+volume (optional): the volume of the AUV in cubic meters. The default value is 0.1 m^3
+thruster_distance (optional): the distance from the center of mass of the AUV to the thruster in meters. The default value is 0.5m.
+The function should return the acceleration of the AUV in a tuple (x, y) in meters per second squared."""
+
+
+def calculate_auv_acceleration(
+    F_magnitude, F_angle, mass=100, volume=0.1, thruster_distance=0.5
+):
+    if mass <= 0 or volume <= 0:
+        raise ValueError("Mass or volume cannot be negative or 0")
+    return (
+        F_magnitude * math.cos(F_angle) / mass,
+        F_magnitude * math.sin(F_angle) / mass,
+    )
+
+
+"""Problem 8 (part 2)
+Write a Python function, calculate_auv_angular_acceleration, that calculates the angular acceleration of the AUV.
+The function should take the following arguments:
+F_magnitude: the magnitude of force applied by the thruster in Newtons.
+F_angle: the angle of the force applied by the thruster in radians.
+inertia (optional): the moment of inertia of the AUV in kg/m^2. The default value is 0.1 m^3
+thruster_distance (optional): the distance from the center of mass of the AUV to the thruster in meters. The default value is 0.5 m
+The function should return the angular acceleration of the AUV in radians per second squared."""
+
+
+def calculate_auv_angular_acceleration(
+    F_magnitude, F_angle, inertia=0.1, thruster_distance=0.5
+):
+    if inertia <= 0:
+        raise ValueError("Inertia cannot be negative or 0")
+    tau = calculate_torque(F_magnitude, math.degrees(F_angle), thruster_distance)
+    print(tau)
+    angular_acceleration = calculate_angular_acceleration(tau, inertia)
+    return myRound(angular_acceleration)
+
+
+"""Problem 9
+Write a Python function, calculate_auv2_acceleration, that calculates the acceleration of the AUV in the 2D plane.
+The function should take the following arguments:
+T: an np.ndarray of the magnitudes of the forces applied by the thrusters in Newtons.
+alpha: the angle of the thrusters in radians.
+theta: the angle of the AUV in radians.
+mass (optional): the mass of the AUV in kilograms. The default value is 100kg.
+Will return a tuple of the forces in the X-direction and Y-direction in m/s^2"""
+
+
+def calculate_auv2_acceleration(forces, alpha, theta, mass=100):
+    if mass <= 0:
+        raise ValueError("Mass cannot be less than zero")
+    alpha_rotation_matrix = np.copy(
+        [
+            [math.cos(alpha), math.cos(alpha), -math.cos(alpha), -math.cos(alpha)],
+            [math.sin(alpha), -math.sin(alpha), -math.sin(alpha), math.sin(alpha)],
+        ]
+    )
+    theta_rotation_matrix = np.copy(
+        [[math.cos(theta), -math.sin(theta)], [math.sin(theta), math.cos(theta)]]
+    )
+    world_forces = np.matmul(alpha_rotation_matrix, forces)
+    return np.matmul(world_forces, theta_rotation_matrix)
+
+
+"""Write a Python function, calculate_auv2_angular_acceleration, that calculates the angular acceleration of the AUV.
+The function should take the following arguments:
+T: an np.ndarray of the magnitudes of the forces applied by the thrusters in Newtons.
+alpha: the angle of the thrusters in radians.
+L: the distance from the center of mass of the AUV to the thrusters in meters.
+l: the distance from the center of mass of the AUV to the thrusters in meters.
+inertia (optional): the moment of inertia of the AUV in kg⋅m^2. The default value is 100kg⋅m^2
+The function should return the angular acceleration of the AUV in radians per second squared."""
+
+
+def calculate_auv2_angular_acceleration(forces, alpha, L, l, inertia=100):
+    if L <= 0 or l <= 0 or inertia <= 0:
+        raise ValueError(
+            "Distance is a magnitude cannot be 0 or negative, inertia cannot be 0 or negative"
+        )
+    netForce = 0
+    for x in range(0, len(forces)):
+        netForce += forces[x] if x % 2 == 0 else -forces[x]
+    netTau = (L * math.sin(alpha) + l * math.cos(alpha)) * (netForce)
+    return netTau / inertia
--- test_physics.py
+++ test_physics.py
@@ -1,19 +1,150 @@
 import unittest
-import physics
+import physics  # IF ITS IN A CLASS I CAN INSTEAD TYPE: import physics.physics as physics
+import numpy as np
 
 
 class TestPhysics(unittest.TestCase):
     def test_calculate_buoyancy(self):
         self.assertEqual(physics.calculate_buoyancy(10, 10), 981)
         self.assertNotEqual(physics.calculate_buoyancy(10, 10), 1000)
-        self.assertEqual(physics.calculate_buoyancy(15, 2.3), 338.445)
+        with self.assertRaises(ValueError):
+            physics.calculate_buoyancy(-1, 2.3)
+        # Another way to assertRaises
+        # self.assertRaises(ValueError, lambda: physics.calculate_buoyancy(0,0))
 
     def test_will_it_float(self):
-        self.assertTrue(physics.will_it_float(10, 10))
-        self.assertFalse(physics.will_it_float(0.01, 100))
-        self.assertFalse(physics.will_it_float(0.999, 1000))
+        self.assertTrue(physics.will_it_float(10, 10))  # will float
+        self.assertEqual(
+            physics.will_it_float(0.01, 10), "NEUTRAL BUOYANCY"
+        )  # neutral buoyancy
+        self.assertFalse(physics.will_it_float(0.999, 1000))  # will sink
+        with self.assertRaises(ValueError):
+            physics.will_it_float(-1, 2.3)
 
     def test_calculate_pressure(self):
-        self.assertEqual(physics.calculate_pressure(20), 196200)
+        self.assertEqual(physics.calculate_pressure(20), 297525.0)
         self.assertNotEqual(physics.calculate_pressure(20), 200000)
-        self.assertEqual(physics.calculate_pressure(0), 0)
+        with self.assertRaises(ValueError):
+            physics.calculate_pressure(-1)
+        with self.assertRaises(ValueError):
+            physics.calculate_pressure(0)
+
+    def test_calculate_acceleration(self):
+        self.assertEqual(physics.calculate_acceleration(5, 2), 2.5)
+        self.assertNotEqual(physics.calculate_acceleration(5, 2), 3)
+        self.assertEqual(physics.calculate_acceleration(-5, 2), -2.5)
+        self.assertRaises(ValueError, lambda: physics.calculate_acceleration(5, 0))
+
+    def test_calculate_angular_acceleration(self):
+        self.assertEqual(physics.calculate_angular_acceleration(5, 2), 2.5)
+        self.assertNotEqual(physics.calculate_angular_acceleration(5, 2), 3)
+        self.assertEqual(physics.calculate_angular_acceleration(-5, 2), -2.5)
+        self.assertRaises(
+            ValueError, lambda: physics.calculate_angular_acceleration(5, 0)
+        )
+
+    def test_calculate_torque(self):
+        self.assertEqual(physics.calculate_torque(5, 2, 30), 5.235)
+        self.assertNotEqual(physics.calculate_torque(5, 2, 30), -5.33)
+        self.assertEqual(physics.calculate_torque(5, 90, 2), 10)
+        self.assertEqual(physics.calculate_torque(-2, 2, 2), -0.14)
+        self.assertEqual(physics.calculate_torque(2, -10, 2), -0.695)
+        self.assertRaises(ValueError, lambda: physics.calculate_torque(5, 2, -1))
+
+    def test_calculate_moment_of_inertia(self):
+        self.assertEqual(physics.calculate_moment_of_inertia(5, 2), 20)
+        self.assertNotEqual(physics.calculate_moment_of_inertia(5, 2), 21)
+        self.assertEqual(physics.calculate_moment_of_inertia(5, -2), 20)
+        self.assertRaises(
+            ValueError, lambda: physics.calculate_moment_of_inertia(-5, 2)
+        )
+
+    def test_calculate_auv_acceleration(self):
+        self.assertEqual(physics.calculate_auv_acceleration(5, np.pi / 2, 2), (0, 2.5))
+        self.assertNotEqual(physics.calculate_auv_acceleration(5, 0, 2), (2.5, 0))
+        self.assertEqual(
+            physics.calculate_auv_acceleration(
+                500, np.pi / 4, volume=100, thruster_distance=100
+            ),
+            (2.5 * np.sqrt(2), 2.5 * np.sqrt(2)),
+        )
+        self.assertNotEqual(
+            physics.calculate_auv_acceleration(
+                500, 1, volume=100, thruster_distance=100
+            ),
+            (2, 2),
+        )
+        self.assertRaises(
+            ValueError, lambda: physics.calculate_auv_acceleration(5, 1, -5, 5)
+        )
+        self.assertRaises(
+            ValueError, lambda: physics.calculate_auv_acceleration(5, 1, 5, -5)
+        )
+        self.assertRaises(
+            ValueError, lambda: physics.calculate_auv_acceleration(5, 1, -5, -5)
+        )
+
+    def test_calculate_auv_angular_acceleration(self):
+        self.assertEqual(physics.calculate_auv_angular_acceleration(5, 1, 2), 1.052)
+        self.assertNotEqual(physics.calculate_auv_angular_acceleration(5, 1, 2), 1)
+        self.assertEqual(
+            physics.calculate_auv_angular_acceleration(5, np.pi / 2, 5, 15), 15
+        )
+        self.assertNotEqual(
+            physics.calculate_auv_angular_acceleration(5, np.pi / 2, 5, 15), 10
+        )
+        self.assertRaises(
+            ValueError,
+            lambda: physics.calculate_auv_angular_acceleration(5, np.pi / 2, -1, 10),
+        )
+
+    def test_calculate_auv2_acceleration(self):
+        testOne = physics.calculate_auv2_acceleration([20, 20, 10, 10], np.pi / 4, 0)
+        self.assertTrue(
+            np.allclose(
+                testOne,
+                [10 * np.sqrt(2), 0],
+            )
+        )
+        self.assertFalse(np.allclose(testOne, [10, 0]))
+
+        testTwo = physics.calculate_auv2_acceleration(
+            [40, 20, 30, 20], np.pi / 3, np.pi / 6, 500
+        )  # π/6 is 30˚
+        self.assertTrue(np.allclose(testTwo, [5 * np.sqrt(3), 5]))
+        self.assertFalse(np.allclose(testTwo, [10 * np.sqrt(3), 10]))
+
+        testThree = physics.calculate_auv2_acceleration(
+            [-40, -20, 30, 20], np.pi / 3, np.pi / 6
+        )
+        self.assertTrue(np.allclose(testThree, [-70 / 2 * np.sqrt(3), 5]))
+        self.assertFalse(np.allclose(testThree, [-140 / 2 * np.sqrt(3), 10]))
+        self.assertRaises(
+            ValueError,
+            lambda: physics.calculate_auv2_acceleration(
+                [-40, -20, 30, 20], np.pi / 3, np.pi / 6, -20
+            ),
+        )
+
+    def test_calculate_auv2_angular_acceleration(self):
+        testOne = physics.calculate_auv2_angular_acceleration(
+            [40, 20, 30, 10], np.pi / 2, 10, 5
+        )
+        self.assertEqual(testOne, 4.0)
+        self.assertNotEqual(testOne, 20)
+
+        testTwo = physics.calculate_auv2_angular_acceleration(
+            [40, 20, 30, 10], 3 * np.pi / 2, 10, 5
+        )
+        self.assertAlmostEqual(testTwo, -4.0)
+        self.assertNotAlmostEqual(testTwo, 4.0)
+
+        self.assertRaises(
+            ValueError,
+            lambda: physics.calculate_auv2_angular_acceleration(
+                [40, 20, 30, 10],
+                np.pi,
+                -1,
+                0,
+            ),
+        )
